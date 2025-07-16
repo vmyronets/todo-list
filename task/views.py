28ab6c58e9a5c316eeca2b73e5032f3eaf8fe11e
Volodymyr Myronets <volodymyrmyronets@gmail.com>
2025-07-16 18:52:19 +0300

    Create class TaskListView in views.py for displaying tasks' list

--- task/views.py
+++ task/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse_lazy
+from django.views import generic
 
-# Create your views here.
+from task.models import Task, Tag
+
+
+def index(request):
+    tasks = Task.objects.all().order_by("is_done", "-created")
+    return render(
+        request,
+        template_name="task/index.html",
+        context={"tasks": tasks}
+    )
+
+
+class TaskListView(generic.ListView):
+    model = Task
+    template_name = "task/task_list.html"
+    context_object_name = "task_list"
+
+    def get_queryset(self):
+        return Task.objects.order_by("is_done", "-created")
+
+
