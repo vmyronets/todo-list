08127a946a6cf027b6d59ebbdb67045b8b55f700
Volodymyr Myronets <volodymyrmyronets@gmail.com>
2025-07-16 16:32:17 +0300

    Registered models in admin panel

--- task/admin.py
+++ task/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from task.models import Task, Tag
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ["content", "created_at", "complete_by", "completed"]
+    list_filter = ["completed", "tags"]
+    search_fields = ["content"]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+    search_fields = ["name"]
