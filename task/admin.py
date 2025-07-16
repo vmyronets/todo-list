a2a800065e7adf46eac2e9c1f59a151f4a22e43d
Volodymyr Myronets <volodymyrmyronets@gmail.com>
2025-07-16 17:26:45 +0300

    Add models to task/models.py

--- task/admin.py
+++ task/admin.py
@@ -5,8 +5,8 @@ from task.models import Task, Tag
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ["content", "created_at", "complete_by", "completed"]
-    list_filter = ["completed", "tags"]
+    list_display = ["content", "created", "deadline", "is_done"]
+    list_filter = ["is_done", "tags"]
     search_fields = ["content"]
 
 
--- task/models.py
+++ task/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=255, unique=True)
+    name = models.CharField(max_length=50, unique=True)
 
     def __str__(self):
         return self.name
@@ -10,10 +10,13 @@ class Tag(models.Model):
 
 class Task(models.Model):
     content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    complete_by = models.DateTimeField(null=True, blank=True)
-    completed = models.BooleanField(default=False)
-    tags = models.ManyToManyField("Tag", related_name="tasks")
+    created = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(null=True, blank=True)
+    is_done = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag, related_name="tasks")
+
+    class Meta:
+        ordering = ["is_done", "-created"]
 
     def __str__(self):
         return self.content[:30] + "..."
